463b9285cf40e9adc59904e2ec1a211d1782f400
zx7898 <mm@88889.x10.mx>
2025-11-20 16:38:13 +0800

    Update argo.sh

--- argo.sh
+++ argo.sh
@@ -439,8 +439,7 @@ After=network.target
 Type=simple
 NoNewPrivileges=yes
 TimeoutStartSec=0
-ExecStart=/bin/sh -c "/etc/sing-box/argo tunnel --url http://localhost:8001 --no-autoupdate --edge-ip-version auto --protocol http2 > /etc/sing-box/argo.log 2>&1"
-Restart=on-failure
+ExecStart=/bin/sh -c "/etc/sing-box/argo tunnel --edge-ip-version auto --config /etc/sing-box/tunnel.yml run > /etc/sing-box/argo.log 2>&1"Restart=on-failure
 RestartSec=5s
 
 [Install]
